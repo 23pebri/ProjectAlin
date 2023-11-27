078c2fbef808e1f9ef9a9f6cf5ca6082d5416013
Alvi399 <muhammadalvi.23339@mhs.unesa.ac.id>
2023-11-27 12:59:01 +0700

    perubahan

--- main.cpp
+++ main.cpp
@@ -9,34 +9,55 @@ int PersSatuVar() {
 }
 int PersDuaVar() {
     //digarap alvi
-    //ax + by = c
-    //dx + ey = f
-    int x1,x2,y1,y2,a,b,c,d,e,f;
-    cout << "ax1 + by1 = c" << endl;
-    cout << "dx2 + ey1 = f" << endl;
-    cout << "masukan nilai dari x1 : ";
-    cin >> x1;
-    cout << "masukan nilai dari a : ";
-    cin >> a;
-    cout << "masukan nilai dari y1 : ";
-    cin >> y1;
-    cout << "masukan nilai dari b : ";
-    cin >> b;
-    cout << "masukan nilai dari c : ";
-    cin >> c;
-
-    cout << "masukan nilai dari x2 : ";
-    cin >> x2;
-    cout << "masukan nilai dari d : ";
-    cin >> d;
-    cout << "masukan nilai dari y2 : ";
-    cin >> y2;
-    cout << "masukan nilai dari e : ";
-    cin >> e;
-    cout << "masukan nilai dari f : ";
-    cin >> f;
-
+    int x[3],y[3],z[3];
+    float hz,hy,hx,hslx,hsly;
+    cout<<"Mencari Persamaan Linier\ndengan bentuk :\n";
+    cout<<"x1 + y1 = z1\n";
+    cout<<"x2 + y2 = z2\n";
+    cout<<"========================\n";
+    for(int i=1;i<3;i++)
+    {
+        cout<<"Masukkan Nilai x"<<i<<" : ";cin>>x[i];
+        cout<<"Masukkan Nilai y"<<i<<" : ";cin>>y[i];
+        cout<<"Masukkan Nilai z"<<i<<" : ";cin>>z[i];
+    }
+    cout<<"\nSehingga menjadi :\n";
+    cout<<x[1]<<"x + "<<y[1]<<"y = "<<z[1]<<"\n";
+    cout<<x[2]<<"x + "<<y[2]<<"y = "<<z[2]<<"\n";
+    if(x[1] == x[2])
+    {
+        int m;
+        hy= y[1]-y[2];
+        hz= z[1]-z[2];
+        hsly= hz / hy;
+        m= hsly * y[1];
+        hx= z[1]- m;
+        hslx= hx / x[1];
+    }
+    else
+    {
+        int m,mx,my;
+        float n,x1,y1,z1,x2,y2,z2;
+        m= x[2] * x[1];
+        mx= m / x[1];
+        my= m / x[2];
+        y1= y[1] * mx;
+        z1= z[1] * mx;
+        y2= y[2] * my;
+        z2= z[2] * my;
+        hy= y1-y2;
+        hz= z1-z2;
+        hsly= hz / hy;
+        n= hsly * y[1];
+        hx = z[1] -  n;
+        hslx= hx / x[1];
+    }
+    cout<<"\nJawabannya adalah :\n";
+    cout<<"Nilai x = "<<hslx<<"\n";
+    cout<<"Nilai y = "<<hsly<<"\n";
+    
 }
+
 int PersTigaVar() {
     //digarap devi dan najwa
 }
